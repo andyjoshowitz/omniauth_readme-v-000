dab2141febb2b307e092acaeacfbe468f3549c12
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-13 13:25:41 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  root 'welcome#home'
+  root 'welcome#home
+  get '/auth/github/callback' => 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
